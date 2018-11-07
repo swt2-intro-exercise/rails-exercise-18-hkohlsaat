476bef72cba7da9415327e62b27c574cd71a6a36
Hannes Kohlsaat <hkohlsaat@gmail.com>
2018-11-07 15:58:08 +0100

    failing test for title presence on papers; problem #6

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should validate title to be present" do
+    paper = Paper.new(venue: "HPI, Potsdam, Germany", year: 2003)
+    expect(paper).not_to be_valid
+  end
+
 end
