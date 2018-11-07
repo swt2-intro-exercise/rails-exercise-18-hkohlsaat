57018f740e910ed596033fb507c8cd413ddfca2a
Hannes Kohlsaat <hkohlsaat@gmail.com>
2018-11-07 16:01:27 +0100

    faling test for venue of paper to be present; problem #6

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe Paper, type: :model do
     expect(paper).not_to be_valid
   end
 
+  it "should validate venue to be present" do
+    paper = Paper.new(title: "Computer Science in rough times", year: 2003)
+    expect(paper).not_to be_valid
+  end
+
 end
