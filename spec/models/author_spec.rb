e816f4db7b5d81284af03e0fa5ff45252a6eb88c
Hannes Kohlsaat <hkohlsaat@gmail.com>
2018-11-07 15:28:28 +0100

    testing full name capability of Author; problem #3

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should give both first and last name in the (full) name" do
+    author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "https://de.wikipedia.org/wiki/Alan_Turing")
+    expect(author.name).to eq("Alan Turing")
+  end
+
 end
